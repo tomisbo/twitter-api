cb96d2ee46375807c3201145b6265e99e2747a90
Sébastien Saunier <seb@saunier.me>
2018-11-30 17:02:54 +0100

    Add Celery

---
+++ app/celery.py
@@ -0,0 +1,27 @@
+# Run with:
+#   `pipenv run celery -A app.celery worker`
+
+from celery import Celery
+from app import create_app
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(create_app())
+
+@celery.task()
+def add_together(a, b):
+    return a + b
--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    CELERY_RESULT_BACKEND = os.environ['CELERY_RESULT_BACKEND']
+    CELERY_BROKER_URL = os.environ['CELERY_BROKER_URL']
+
